8326763392781e2e1954d7d7965e8fcc06f24f61
Kari Narhi <Kari.Narhi24@hotmail.com>
2020-08-10 22:57:03 +0300

    Added NavBar

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import AppNavBar from "./components/AppNavBar";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <AppNavBar />
     </div>
   );
 }
