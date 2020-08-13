633ea8b679d65ee1e8c265fa86a5827a59b13767
Kari Narhi <Kari.Narhi24@hotmail.com>
2020-08-13 16:39:33 +0300

    Implemented axios and connected app to MongoDB

--- client/src/reducers/itemReducer.js
+++ client/src/reducers/itemReducer.js
@@ -1,13 +1,13 @@
-import { v4 as uuidv4 } from "uuid";
-import { GET_ITEMS, ADD_ITEM, DELETE_ITEM } from "../actions/types";
+import {
+  GET_ITEMS,
+  ADD_ITEM,
+  DELETE_ITEM,
+  ITEMS_LOADING,
+} from "../actions/types";
 
 const initialState = {
-  items: [
-    { id: uuidv4(), name: "Eggs" },
-    { id: uuidv4(), name: "Bacon" },
-    { id: uuidv4(), name: "Steak" },
-    { id: uuidv4(), name: "Beer" },
-  ],
+  items: [],
+  loading: false,
 };
 
 export default function (state = initialState, action) {
@@ -15,18 +15,26 @@ export default function (state = initialState, action) {
     case GET_ITEMS:
       return {
         ...state,
+        items: action.payload,
+        loading: false,
       };
 
     case DELETE_ITEM:
       return {
         ...state,
-        items: state.items.filter((item) => item.id !== action.payload),
+        items: state.items.filter((item) => item._id !== action.payload),
       };
 
     case ADD_ITEM:
       return {
         ...state,
         items: [action.payload, ...state.items],
+      };
+
+    case ITEMS_LOADING:
+      return {
+        ...state,
+        loading: true,
       };
 
     default:
