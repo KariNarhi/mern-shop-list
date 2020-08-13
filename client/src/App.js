c6bd639be4ae5df30200729eb5da9881800093ef
Kari Narhi <Kari.Narhi24@hotmail.com>
2020-08-13 14:37:07 +0300

    Implement Redux 2/2

--- client/src/App.js
+++ client/src/App.js
@@ -3,16 +3,21 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import { Provider } from "react-redux";
 import store from "./store";
+import { Container } from "reactstrap";
 
 import AppNavBar from "./components/AppNavBar";
 import ShoppingList from "./components/ShoppingList";
+import ItemModal from "./components/ItemModal";
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
         <AppNavBar />
-        <ShoppingList />
+        <Container>
+          <ItemModal />
+          <ShoppingList />
+        </Container>
       </div>
     </Provider>
   );
