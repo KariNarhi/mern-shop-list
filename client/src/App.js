cb5d2b49d5e9c2b222e4db99c7555471332dec3e
Kari Narhi <Kari.Narhi24@hotmail.com>
2020-08-15 18:38:32 +0300

    React/Redux auth state

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,33 @@
-import React from "react";
+import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import { Provider } from "react-redux";
-import store from "./store";
 import { Container } from "reactstrap";
+import store from "./store";
+import { loadUser } from "./actions/authActions";
 
 import AppNavBar from "./components/AppNavBar";
 import ShoppingList from "./components/ShoppingList";
 import ItemModal from "./components/ItemModal";
 
-function App() {
-  return (
-    <Provider store={store}>
-      <div className="App">
-        <AppNavBar />
-        <Container>
-          <ItemModal />
-          <ShoppingList />
-        </Container>
-      </div>
-    </Provider>
-  );
+class App extends Component {
+  componentDidMount() {
+    store.dispatch(loadUser);
+  }
+
+  render() {
+    return (
+      <Provider store={store}>
+        <div className="App">
+          <AppNavBar />
+          <Container>
+            <ItemModal />
+            <ShoppingList />
+          </Container>
+        </div>
+      </Provider>
+    );
+  }
 }
 
 export default App;
